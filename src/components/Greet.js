176539d25261a7aa67885ab1dd0497e4523aeb56
Bappi Kabir <kabirbapson@gmail.com>
2022-04-22 01:43:56 +0100

    minor

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,7 +3,7 @@ import React from "react";
 const Greet = () => {
     return <div>
         <h1>Hello Abu Lameen</h1>
-        <p>Am here</p>
+        {/* <p>Am here</p> */}
     </div>
 }
 export default Greet;
